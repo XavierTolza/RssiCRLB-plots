8848570e1ef3aa68253ad3d9e6fef606bfb135b1
Xavier Tolza <tolza.xavier@gmail.com>
2018-07-26 21:51:06 +0200

    added function to plot curves

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import os
 
 import pandas
 from pandas import DataFrame
+from bokeh.plotting import figure, output_file, show
+import numpy as np
 
 csv_name = "ffly_reu_measurements_auto_placement.csv"
 
 def convert_measurements():
-    path = "/media/xavier/Data/Code/Pycharm/indoor_loc/Data/Measurements/"
+    path = "Measurements/"
     files = [i for i in os.listdir(path) if "ffly_reu" in i]
     jsons = {}
     for file in files:
@@ -34,11 +36,29 @@ def convert_measurements():
 
 
 def plot_measurements():
-    df = pandas.read_csv(csv_name)
+    measures = ["%i mean"%i for i in np.arange(3,8)]
+    types = "real,cramer,simu".split(",")
+    root = "exported_data"
 
-    for mode, df_mode in df.groupby("placement_mode"):
-        for
+    output_file("plot.html",title="Publication 2 plot")
+    p = figure(title="Cumulative density function of positionning error", x_axis_label='Distance in meters',
+               y_axis_label='CDF',sizing_mode="stretch_both")
+
+    import bokeh.palettes as palettes
+    colors = np.reshape(palettes.Category20b[20],(-1,4))
+
+    for measure_i, measure in enumerate(measures):
+        for typ_i, typ in enumerate(types):
+            filename = "%s/%s_%s_cep.csv" % (root,measure,typ)
+            _,x,y = pandas.read_csv(filename).values.transpose()
+            l = p.line(x, y, legend="%s %s" % (measure, typ), line_width=2, color=colors[measure_i,typ_i],muted_alpha=0)
+            l.muted=measure_i>0
+
+    p.legend.location = "top_left"
+    p.legend.click_policy="mute"
+    show(p)
+
 
 
 if __name__ == '__main__':
-    convert_measurements()
+    plot_measurements()
