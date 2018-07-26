0e4f9ce772cdca16a512ab8a8e978693e7d909c0
Xavier Tolza <tolza.xavier@gmail.com>
2018-07-26 22:20:21 +0200

    finished function plot ad updated data

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import os
 
 import pandas
+from bokeh import events
+from bokeh.models import CustomJS
 from pandas import DataFrame
 from bokeh.plotting import figure, output_file, show
 import numpy as np
@@ -36,7 +38,7 @@ def convert_measurements():
 
 
 def plot_measurements():
-    measures = ["%i mean"%i for i in np.arange(3,8)]
+    measures = ["non optimal"]+["%i mean"%i for i in np.arange(3,8)]
     types = "real,cramer,simu".split(",")
     root = "exported_data"
 
@@ -45,17 +47,28 @@ def plot_measurements():
                y_axis_label='CDF',sizing_mode="stretch_both")
 
     import bokeh.palettes as palettes
-    colors = np.reshape(palettes.Category20b[20],(-1,4))
+    colors = np.reshape(palettes.Category20b[20]+palettes.Category20c[20][4:],(-1,4))
+
+    def plot(x, y, muted, color_index, color_subindex, legend,lw=2,ls="solid"):
+        l = p.line(x, y, legend=legend, line_width=lw, color=colors[color_index,color_subindex],
+                   muted_alpha=0,line_dash=ls)
+        l.muted=muted
+
 
     for measure_i, measure in enumerate(measures):
-        for typ_i, typ in enumerate(types):
+        for typ_i, (typ,lw,ls) in enumerate(zip(types,[2,1.5,1.5],"solid,dashed,dotted".split(","))):
             filename = "%s/%s_%s_cep.csv" % (root,measure,typ)
             _,x,y = pandas.read_csv(filename).values.transpose()
-            l = p.line(x, y, legend="%s %s" % (measure, typ), line_width=2, color=colors[measure_i,typ_i],muted_alpha=0)
-            l.muted=measure_i>0
+            plot(x,y,measure_i>0,measure_i,typ_i,"%s %s" % (measure.replace("mean","AP").replace("non","7 AP non"), typ),lw=lw,ls=ls)
+    filename = "%s/random_cep.csv" % root
+    _,x,y = pandas.read_csv(filename).values.transpose()
+    plot(y,x,True,measure_i+1,0,"random")
 
     p.legend.location = "top_left"
     p.legend.click_policy="mute"
+    def show_hide_legend(legend=p.legend[0]):
+        legend.visible = not legend.visible
+    p.js_on_event(events.DoubleTap, CustomJS.from_py_func(show_hide_legend))
     show(p)
 
 
