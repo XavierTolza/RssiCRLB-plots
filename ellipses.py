e773948e19946aa3d4ed189bbfe85232c871c761
Xavier Tolza <tolza.xavier@gmail.com>
2019-02-08 16:02:45 +0100

    Improved plot a bit and uploaded cache

--- ellipses.py
+++ ellipses.py
@@ -1,17 +1,16 @@
 import itertools
-import os
+import lzma
 
 import dill
-import matplotlib.pyplot as plt
 import numpy as np
+import os
 from bokeh.io import show
-from tqdm import tqdm
 
 from plot_results import plot_results
 
-cache_file = "/tmp/cache.pkl"
+cache_file = "cache.pkl"
 if os.path.isfile(cache_file):
-    with open(cache_file,"rb") as fp:
+    with lzma.open(cache_file,"rb") as fp:
         transmitters_coordinates, estimate, ap_coordinates, crlb = dill.load(fp)
 
 else:
@@ -26,51 +25,46 @@ else:
     variance = 2
 
     J, I, N = len(ap_coordinates), len(transmitters_coordinates), 20
-    n_configurations = [10]*50
-    estimates = []
-    for n_configuration in tqdm(n_configurations):
-        (G, noise_G), (a0, noise_a0), (R, noise_R) = [
-            (np.array([i[0]] * j), np.random.normal(0, np.sqrt(i[1]), (n_configuration, j)))
-            for i, j in
-            zip([transmitters_gains, a0_muvar, receiver_gains], (J, 1, I))]
-        noise = np.random.normal(0, np.sqrt(variance), (n_configuration, J, I, N))
-        noise = noise_G[:, :, None, None] - noise_R[:, None, :, None] + noise_a0[:, 0, None, None, None] + noise
-
-        Gar = G[:, None] + a0[0] - R[None, :]
-        vector = ap_coordinates[:, None, :] - transmitters_coordinates[None, :, :]
-        d = np.linalg.norm(vector, axis=2)
-        Yb = Gar - 10 * gamma * np.log(d) / np.log(10)
-        Y = Yb[None, :, :, None] + noise
-
-        # Calcul des estimes
-        itertools_input = [np.arange(-i, 2 * i, step_size) for i in room_shape]
-        theta = np.array(list(itertools.product(*itertools_input)))
-        theta_len = np.prod([len(i) for i in itertools_input])
+    n_configuration = 100
 
 
-        dtheta = np.linalg.norm(ap_coordinates[:, None, :] - theta[None, :, :], axis=2)
-        Ybtheta = Gar[:, :, None] - 10 * gamma * np.log(dtheta[:, None, :]) / np.log(10)
-        error = Y[:, :, :, :, None] - Ybtheta[None, :, :, None, :]
+    (G, noise_G), (a0, noise_a0), (R, noise_R) = [
+        (np.array([i[0]] * j), np.random.normal(0, np.sqrt(i[1]), (n_configuration, j)))
+        for i, j in
+        zip([transmitters_gains, a0_muvar, receiver_gains], (J, 1, I))]
+    noise = np.random.normal(0, np.sqrt(variance), (n_configuration, J, I, N))
+    noise = noise_G[:, :, None, None] - noise_R[:, None, :, None] + noise_a0[:, 0, None, None, None] + noise
 
-        # On calcule le likelihood
-        error = np.moveaxis(np.moveaxis(error, 4, 1), 3, 2).reshape((-1, I, J * N))
-        receiver, _ = np.array(list(itertools.product(range(J), range(N)))).T
-        cov = (receiver[:, None] == receiver[None, :]) * receiver_gains[1]
-        cov = cov + a0_muvar[1] + transmitters_gains[1]
-        cov = cov + np.eye(J * N) * variance
+    Gar = G[:, None] + a0[0] - R[None, :]
+    vector = ap_coordinates[:, None, :] - transmitters_coordinates[None, :, :]
+    d = np.linalg.norm(vector, axis=2)
+    Yb = Gar - 10 * gamma * np.log(d) / np.log(10)
+    Y = Yb[None, :, :, None] + noise
 
-        covi = np.linalg.inv(cov)
+    # Calcul des estimes
+    theta = np.array(list(itertools.product(*[np.arange(-i, 2 * i, step_size) for i in room_shape])))
+    d_theta = np.linalg.norm(ap_coordinates[:, None, :] - theta[None, :, :], axis=2)
+    Yb_theta = Gar[:, :, None] - 10 * gamma * np.log(d_theta[:, None, :]) / np.log(10)
+    error = Y[:, :, :, :, None] - Yb_theta[None, :, :, None, :]
 
-        likelihood_list = []
-        for i in range(I):
-            likelihood = [e.T.dot(covi).dot(e) for e in error[:, i, :]]
-            likelihood = np.reshape(likelihood, (n_configuration, theta_len))
-            likelihood_list.append(likelihood)
-        likelihood = np.array(likelihood_list)
-        argmin = np.nanargmin(likelihood, axis=2).T
-        estimates.append(theta[argmin, :])
+    # On calcule le likelihood
+    error = np.moveaxis(np.moveaxis(error, 4, 1), 3, 2).reshape((-1, I, J * N))
+    receiver, _ = np.array(list(itertools.product(range(J), range(N)))).T
+    cov = (receiver[:, None] == receiver[None, :]) * receiver_gains[1]
+    cov = cov + a0_muvar[1] + transmitters_gains[1]
+    cov = cov + np.eye(J * N) * variance
 
-    estimate = np.concatenate(estimates,axis=0)
+    covi = np.linalg.inv(cov)
+
+    likelihood_list = []
+    for i in range(I):
+        likelihood = [e.T.dot(covi).dot(e) for e in error[:, i, :]]
+        likelihood = np.reshape(likelihood, (n_configuration, len(theta)))
+        likelihood_list.append(likelihood)
+    likelihood = np.array(likelihood_list)
+    argmin = np.nanargmin(likelihood, axis=2).T
+    estimate = theta[argmin, :]
+
     estimate_error = estimate - transmitters_coordinates[None, :, :]
 
     # Compute CRLB
@@ -79,7 +73,8 @@ else:
     dYb = np.repeat(np.moveaxis(dYb, 1, 0), N, axis=1)
     fisher = (i.T.dot(covi).dot(i) for i in dYb)
     crlb = np.array([np.linalg.inv(i) for i in fisher])
-    with open(cache_file,"wb") as fp:
+
+    with lzma.open(cache_file,"wb") as fp:
         dill.dump((transmitters_coordinates, estimate, ap_coordinates, crlb),fp)
 
 
--- plot_results.py
+++ plot_results.py
@@ -12,16 +12,21 @@ colors = palettes.Greys[6]
 
 
 def plot_results(transmitters_coordinates, estimate, ap_coordinates, crlb, **kwargs):
-    p1 = figure(title="2-Sigma covariance plot", x_axis_label="X_coordinates", y_axis_label="Y coordinates",
+    p1 = figure(title="2-Sigma covariance plot", x_axis_label="X coordinates (meters)", y_axis_label="Y coordinates (meters)",
                 active_scroll="wheel_zoom", plot_width=800, **kwargs)
 
-    p2 = figure(title="RMSE vs CRLB", x_axis_label="Transmitter", y_axis_label="RMSE",
-                active_scroll="wheel_zoom", plot_width=300,**kwargs)
+    p2 = figure(title="RMSE vs CRLB", x_axis_label="Transmitter", y_axis_label="RMSE (meters",
+                active_scroll="wheel_zoom", plot_width=400,**kwargs)
 
     # Drawing covariance ellipses
     mean_cov = [(np.mean(i, axis=0), np.cov(i.T)) for i in np.moveaxis(estimate, 1, 0)]
-    for i, j, c, d in zip(mean_cov, crlb, colors, transmitters_coordinates):
+
+    selector = np.linalg.norm(estimate-np.mean(estimate,axis=0)[None],axis=2)<3
+
+    for index,(i, j, c, d,_selector) in enumerate(zip(mean_cov, crlb, colors, transmitters_coordinates,selector.T)):
         plot_ellipse(i[1], p1, i[0], line_color="black", line_dash="dashed", legend="Simulation", line_width=1.5)
+        selec_cov = np.cov(estimate[_selector,index,:].T)
+        plot_ellipse(selec_cov, p1, i[0], line_color="black", line_dash="dotted", legend="Filtered simulation", line_width=1.5)
         plot_ellipse(j, p1, d, line_color="black", line_dash="solid", legend="CRLB", line_width=2)
 
     for t, c in zip(range(len(transmitters_coordinates)), "circle,x,square".split(",")):
@@ -29,10 +34,15 @@ def plot_results(transmitters_coordinates, estimate, ap_coordinates, crlb, **kwa
                        line_color="grey",line_width=2)
     p1.circle(*ap_coordinates.T, line_color="black", legend="Access points", fill_color="black",size=10)
 
-    RMSE = np.mean(np.linalg.norm(estimate - transmitters_coordinates[None, :, :], axis=2), axis=0)
+    error = estimate - transmitters_coordinates[None, :, :]
+    error = np.linalg.norm(error, axis=2)
+    RMSE = np.mean(error, axis=0)
+    RMSE_filtered = np.nanmean(np.where(selector,error,np.nan),axis=0)
     x = np.arange(len(transmitters_coordinates))
-    p2.vbar(x=x, top=RMSE, legend="RSME (Simulation)", width=.45, fill_color="black", line_color=None)
-    p2.vbar(x=x + .45, top=[np.trace(i) for i in crlb], legend="CRLB", width=.45, fill_color="grey", line_color=None)
+    width = 0.95/3
+    p2.vbar(x=x, top=RMSE, legend="RSME (Simulation)", width=width, fill_color="black", line_color="black")
+    p2.vbar(x=x+width, top=RMSE_filtered, legend="RSME (Filtered simulation)", width=width, fill_color="grey", line_color="black")
+    p2.vbar(x=x + 2*width, top=[np.trace(i) for i in crlb], legend="CRLB", width=width, fill_color="white", line_color="black")
 
     p2.legend.location = "bottom_right"
     p1.legend.location = "center_right"
